0158951af6395fca61f7d275b5591761584fa041
GLK-7 <gabriel.godoitb@gmail.com>
2024-11-14 11:50:41 -0300

    test4

--- src/components/pages/Home.tsx
+++ src/components/pages/Home.tsx
@@ -37,7 +37,7 @@ const Home: React.FC = () => {
         <Timeline2 events={timelineEvents} />
       </Section>
 
-      <img src="https://drive.google.com/thumbnail?id=1pSlGfqqnbnXDgS6pi7GWgRgXLqtDD8GY&sz=s200" alt="teste" />
+      <img src="https://drive.google.com/thumbnail?id=1pSlGfqqnbnXDgS6pi7GWgRgXLqtDD8GY&sz=s1000" alt="teste" />
     </div>
   );
 };
