b5940f4601bd7f83593252a75d604439e93a884c
GLK-7 <gabriel.godoitb@gmail.com>
2024-11-14 11:11:27 -0300

    fix imgs

--- src/components/layout/PhotoCarousel.tsx
+++ src/components/layout/PhotoCarousel.tsx
@@ -1,51 +1,37 @@
-import {useState, useEffect} from 'react';
+import { useState, useEffect } from 'react';
 import Slider from 'react-slick';
 import 'slick-carousel/slick/slick.css';
 import 'slick-carousel/slick/slick-theme.css';
 
-const API_KEY =  import.meta.env.VITE_API_KEY
-const FOLDER_ID =  import.meta.env.VITE_FOLDER_ID
-
-console.log(FOLDER_ID)
+const API_KEY = import.meta.env.VITE_API_KEY;
+const FOLDER_ID = import.meta.env.VITE_FOLDER_ID;
 
 const PhotoCarousel: React.FC = () => {
   const settings = {
     dots: true,
     infinite: true,
     speed: 500,
-    slidesToShow: 3, // Exibir 3 fotos
-    slidesToScroll: 1, // Rolar 1 foto de cada vez
+    slidesToShow: 3,
+    slidesToScroll: 1,
     responsive: [
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          infinite: true,
-          dots: true,
-        },
-      },
-      {
-        breakpoint: 600,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-        },
-      },
+      { breakpoint: 1024, settings: { slidesToShow: 2, slidesToScroll: 1 } },
+      { breakpoint: 600, settings: { slidesToShow: 1, slidesToScroll: 1 } },
     ],
   };
+
   const [images, setImages] = useState<string[]>([]);
+
   useEffect(() => {
     const fetchImages = async () => {
       try {
         const response = await fetch(
-          `https://www.googleapis.com/drive/v3/files?q='${FOLDER_ID}'+in+parents+and+mimeType+contains+'image/'&key=${API_KEY}&fields=files(id,name)`
+          `https://www.googleapis.com/drive/v3/files?q='${FOLDER_ID}'+in+parents+and+mimeType+contains+'image/'&key=${API_KEY}&fields=files(id,name,thumbnailLink)`
         );
         const data = await response.json();
 
-        // Transformar o id em link direto de exibição
+        // Use thumbnailLink se estiver disponível, caso contrário, use o link padrão
         const imageUrls = data.files.map(
-          (file: any) => `https://drive.google.com/uc?id=${file.id}`
+          (file: any) => file.thumbnailLink || `https://drive.google.com/uc?id=${file.id}`
         );
         setImages(imageUrls);
       } catch (error) {
@@ -57,20 +43,11 @@ const PhotoCarousel: React.FC = () => {
   }, []);
 
   return (
-    <div className=" rounded p-4 pb-10">
+    <div className="rounded p-4 pb-10">
       <Slider {...settings}>
         {images.map((photo, index) => (
-          <div
-            key={index}
-            className="flex justify-center items-center h-[500px] p-2"
-          >
-            {' '}
-            {/* Defina uma altura fixa */}
-            <img
-              src={photo}
-              alt={`Foto ${index}`}
-              className="rounded-md w-full h-full object-cover" // Ajuste a largura e altura das imagens
-            />
+          <div key={index} className="flex justify-center items-center h-[300px] p-2">
+            <img src={photo} alt={`Foto ${index}`} className="rounded-md w-full h-full object-cover" />
           </div>
         ))}
       </Slider>
